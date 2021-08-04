f158134c1252e19089de9ad8ae9284ff0d5a2e8b
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 13:53:27 +0545

    commit

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 "use-strict";
+
+//selecting elements
 const player0 = document.querySelector("#score--0");
 const player1 = document.getElementById("score--1");
-const dice = document.querySelector(".dice");
+const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+//starting conditions
 player0.textContent = 0;
 player1.textContent = 0;
+diceEl.classList.add("hidden");
 
-dice.classList.add("hidden");
+//rolling dice functionality
+
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+});
+
+//display dice
+
+//check for no 1; if true, switch to next player

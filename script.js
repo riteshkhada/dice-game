4a18040bb95d5cbec47fb64fa625834da96f1d9b
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 13:10:56 +0545

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use-strict";
 const player0 = document.querySelector("#score--0");
 const player1 = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
 
 player0.textContent = 0;
 player1.textContent = 0;

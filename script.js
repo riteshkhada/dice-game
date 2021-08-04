105392e817f0db244ee71b2d25ef3d9b5738b71f
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 16:57:41 +0545

    commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 "use-strict";
 
 //selecting elements
-const player0 = document.querySelector("#score--0");
-const player1 = document.getElementById("score--1");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
+
+const score0 = document.querySelector("#score--0");
+const score1 = document.getElementById("score--1");
 const currentScore0El = document.getElementById("current--0");
 const currentScore1El = document.getElementById("current--1");
 
@@ -12,8 +15,8 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 //starting conditions
-player0.textContent = 0;
-player1.textContent = 0;
+score0.textContent = 0;
+score1.textContent = 0;
 diceEl.classList.add("hidden");
 
 const scores = [0, 0];
@@ -37,6 +40,9 @@ btnRoll.addEventListener("click", function () {
   } else {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
   }
   //switch player
 });

17a17727daa04d6303a407a610ee3ca3cf21ad43
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 13:58:44 +0545

    commit

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ diceEl.classList.add("hidden");
 
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
-  diceEl.classList.remove("hidden");
 });
 
 //display dice
-
+diceEl.classList.remove("hidden");
+diceEl.src = `dice-${dice}.png`;
 //check for no 1; if true, switch to next player

a29b99d2d6d70699e899e957d215bed5ce346cb1
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 13:05:29 +0545

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use-strict";
+const player0 = document.querySelector("#score--0");
+const player1 = document.getElementById("score--1");
+
+player0.textContent = 0;
+player1.textContent = 0;
+
+dice.classList.add("hidden");

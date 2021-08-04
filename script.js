dbb33e5fe3d93d0d533f2c81990fa3d3df64cb95
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 13:44:08 +0545

    commit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const player0 = document.querySelector("#score--0");
 const player1 = document.getElementById("score--1");
 const dice = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 player0.textContent = 0;
 player1.textContent = 0;
